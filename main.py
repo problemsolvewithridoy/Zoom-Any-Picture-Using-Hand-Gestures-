a66ce2c4f1051e798c6a42e3e71126311c145d5f
Md Ridoy Hossain <inception.helps@gmail.com>
2023-05-14 00:07:19 +0600

    test2

--- main.py
+++ main.py
@@ -16,6 +16,15 @@ while True:
     hands, img = detector.findHands(img)
     
     img1 = cv2.imread("one.jpg")
+
+    if len(hands) == 2:
+        # print("Zoom Gesture")
+        # print(detector.fingersUp(hands[0]), detector.fingersUp(hands[1]))
+        hand1 = hands[0]
+        hand2 = hands[1]
+
+        hand1_fingers = detector.fingersUp(hands[0])
+        hand2_fingers = detector.fingersUp(hands[1])
     
     
 
